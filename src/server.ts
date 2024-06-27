7400cca447a36ea9c9e4b1f38c37619fb2b076c3
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-06-27 16:13:54 -0600

    CORS adjustment

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,7 @@ const corsOptions: CorsOptions = {
     if (origin === process.env.FRONTEND_URL) {
       callback(null, true);
     } else {
+      callback(new Error("CORS Error"));
     }
   },
 };
