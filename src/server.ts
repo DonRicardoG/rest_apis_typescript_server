9e6d88cd8e266fb66a8dc7e8e27388305ef355f4
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-06-27 16:32:31 -0600

    changes on CORS

--- src/server.ts
+++ src/server.ts
@@ -22,17 +22,12 @@ connectDB();
 
 const server = express();
 
-// const corsOptions: CorsOptions = {
-//   origin: function (origin, callback) {
-//     if (origin === process.env.FRONTEND_URL) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("CORS Error"));
-//     }
-//   },
-// };
+const corsOptions: CorsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200,
+};
 
-server.use(cors());
+server.use(cors(corsOptions));
 
 server.use(express.json());
 
